fd22b68fc48331b13864cf00d20dc8be57027c9c
Carter Crews <govolsgo@users.noreply.github.com>
2017-02-04 22:23:39 -0600

    compEdge
    Added compEdge that compares the weight of two edges.

--- haskell/prim.hs
+++ haskell/prim.hs
@@ -1,20 +1,29 @@
 graph = [('a','b',4),('a','h',10),('b','c',8),('b','h',11),('c','d',7),('c','f',4),('c','i',2),('d','e',9),('d','f',14),('e','f',10),('f','g',2),('g','h',1),('g','i',6),('h','i',7)]
 
+-- Returns 1st element from a 3-tuple.
+first (a,_,_) = a
+
+-- Returns 2nd element from a 3-tuple.
+second (_,b,_) = b
+
+-- Returns 3rd element from a 3-tuple.
+third (_,_,c) = c
+
 -- Creates list of 1st elements from a 3-tuple list.
-first [] = []
-first ((a,_,_):list) = a:(first list)
+firstL [] = []
+firstL ((a,_,_):list) = a:(firstL list)
 
 -- Creates list of 2nd elements from a 3-tuple list.
-second [] = []
-second ((_,b,_):list) = b:(second list)
+secondL [] = []
+secondL ((_,b,_):list) = b:(secondL list)
 
 -- Creates list of 3rd elements from a 3-tuple list.
-third [] = []
-third ((_,_,c):list) = c:(third list)
+thirdL [] = []
+thirdL ((_,_,c):list) = c:(thirdL list)
 
 
 -- Prints total weight of graph.
-weight (('a','b',w):list) = sum(third (('a','b',w):list))
+weight (('a','b',w):list) = sum(thirdL (('a','b',w):list))
 
 -- Checks if a vertex from vertex list is connected to a certain edge.
 -- checkVert :: Eq a => [a] -> (a, a, t) -> Bool
@@ -24,3 +33,9 @@ checkVert vertices (a,b,_) = a `elem` vertices || b `elem` vertices
 connEdges vertices [] = []
 connEdges vertices ((a,b,w):list) = if (checkVert vertices (a,b,w)) then ((a,b,w):connEdges vertices list) else (connEdges vertices list)
 
+-- Prints an edge from a list with the smallest weight.
+
+
+-- Compares the weight of two edges.
+compEdge :: Ord b => (a, a, b) -> (a, a, b) -> Ordering
+compEdge a b = compare (third a) (third b)
