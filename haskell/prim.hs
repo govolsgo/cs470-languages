34db899d06ae4b9497157d8e367df18d0f497578
Carter Crews <govolsgo@users.noreply.github.com>
2017-02-04 22:39:57 -0600

    minimalEdge
    Added minimalEdge to return the edge with the smallest weight from a
    list.

--- haskell/prim.hs
+++ haskell/prim.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 graph = [('a','b',4),('a','h',10),('b','c',8),('b','h',11),('c','d',7),('c','f',4),('c','i',2),('d','e',9),('d','f',14),('e','f',10),('f','g',2),('g','h',1),('g','i',6),('h','i',7)]
 
 -- Returns 1st element from a 3-tuple.
@@ -33,9 +35,9 @@ checkVert vertices (a,b,_) = a `elem` vertices || b `elem` vertices
 connEdges vertices [] = []
 connEdges vertices ((a,b,w):list) = if (checkVert vertices (a,b,w)) then ((a,b,w):connEdges vertices list) else (connEdges vertices list)
 
--- Prints an edge from a list with the smallest weight.
-
-
 -- Compares the weight of two edges.
 compEdge :: Ord b => (a, a, b) -> (a, a, b) -> Ordering
 compEdge a b = compare (third a) (third b)
+
+-- Prints an edge from a list with the smallest weight.
+minimalEdge a = minimumBy compEdge a
