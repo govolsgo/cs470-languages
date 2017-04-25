90f8d3a8df62ee0f272c105cc698be9e3c4ddd34
Carter Crews <govolsgo@users.noreply.github.com>
2017-04-25 03:34:55 -0500

    Data storage for the graph that will be read in.

--- c/main.c
+++ c/main.c
@@ -2,26 +2,32 @@
 #include "string.h"
 #include "graph.h"
 #include "prim.h"
+#include "constants.h"
 
-int main ()
+int main()
 {
   char fileName[1000];
   FILE* file;
+  int vertexSize = 0;
+  char vertexNames[NUM_VERTICES][VERT_NAME_LEN];
+  char edges[NUM_VERTICES][NUM_EDGES][2];
+  int edgeSize[NUM_VERTICES] = {0};
   char testInput[25];
 
   getFileName(fileName);
   file = openFile(fileName);
-  
 
-  printf("file=%p\n",file);
-  printf("About to read input file.\n");
-  fscanf(file,"%s",testInput);
+  /* strcpy(vertexNames[50],"Alfa\0"); */
+  
+  readData(&vertexSize,vertexNames,edgeSize,edges,file);
+  
+  /*fscanf(file,"%s",testInput);
 
   while(!feof(file))
     {
       printf("Read: %s\n",testInput);
       fscanf(file,"%s",testInput);
-    }
+    }*/
 
   fclose(file);
   
