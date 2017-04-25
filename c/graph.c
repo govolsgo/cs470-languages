71c15854cc7d500973e1f932bf346b7c1abcf3dc
Carter Crews <govolsgo@users.noreply.github.com>
2017-04-25 03:32:18 -0500

    Added readData to read in the graph. Still working on it. Added findVertIndex to support readData by finding the vertex index in the array.

--- c/graph.c
+++ c/graph.c
@@ -1,5 +1,23 @@
 #include "graph.h"
 
+int findVertIndex(char findMe[], int vertexSize,
+		  char vertexNames[NUM_VERTICES][VERT_NAME_LEN])
+{
+  int index = -1;
+  int i = 0; 
+
+  while(i < NUM_VERTICES && index == -1)
+    {
+      if(strcmp(findMe, vertexNames[i]) == 0)
+	{
+	  index = i;
+	}
+      i++;
+    }
+
+  return index;
+}
+
 void getFileName(char fileName[])
 {
   printf("Enter graph file name: ");
@@ -13,6 +31,7 @@ FILE* openFile(char fileName[])
   FILE* file;
   file = fopen(fileName,"r");
 
+  /* Make sure we opened the file. Retry if not. */
   while(file == NULL)
     {
       printf("Can't open file.\n");
@@ -20,8 +39,28 @@ FILE* openFile(char fileName[])
       file = fopen(fileName,"r");
     }
 
-  printf("file=%p\n",file);
-  printf("Leaving openFile()\n");
-
   return file;
+}
+
+void readData(int* vertexSize, char vertexNames[NUM_VERTICES][VERT_NAME_LEN],
+	      int* edgeSize, char edges[NUM_VERTICES][NUM_EDGES][2],
+	      FILE* file)
+{
+  char tempVertex[VERT_NAME_LEN];
+  int vert1Index = -1;
+  
+  fscanf(file,"%s",tempVertex);
+  vert1Index = findVertIndex(tempVertex,*vertexSize,vertexNames);
+
+  printf("Vertex 1 loc: %d\n",vert1Index);
+
+  /*
+  while(!feof(file))
+    {
+      if()
+	{
+	 
+	}
+    }
+  */
 }
--- c/graph.h
+++ c/graph.h
@@ -1,6 +1,12 @@
 #include "stdio.h"
 #include "string.h"
 #include "prim.h"
+#include "constants.h"
 
+int findVertIndex(char findMe[], int vertexSize,
+		  char vertexNames[NUM_VERTICES][VERT_NAME_LEN]);
 void getFileName(char fileName[]);
 FILE* openFile(char fileName[]);
+void readData(int* vertexSize, char vertexNames[NUM_VERTICES][VERT_NAME_LEN],
+	      int* edgeSize, char edges[NUM_VERTICES][NUM_EDGES][2],
+	      FILE* file);
