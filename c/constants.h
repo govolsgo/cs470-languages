ce35b467cfa9699f4390680959feb5f743e58c46
Carter Crews <govolsgo@users.noreply.github.com>
2017-04-25 03:30:02 -0500

    Added constants.h file to store macros related to array sizes.

---
+++ c/constants.h
@@ -0,0 +1,8 @@
+/* Max number of vertices in the graph. */
+#define NUM_VERTICES 100
+
+/* Max number of edges per vertex. */
+#define NUM_EDGES 10
+
+/* Max length of C strings containing vertex names. */
+#define VERT_NAME_LEN 25
