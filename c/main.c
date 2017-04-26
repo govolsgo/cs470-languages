20a0f1bd30062dde1f9a8026f1c7fffc553d5fcb
Carter Crews <govolsgo@users.noreply.github.com>
2017-04-26 03:29:35 -0500

    Modified the edges variable to account for storing the weight as a C string. Also changed the call to readData to account for the reordering of parameters.

--- c/main.c
+++ c/main.c
@@ -10,16 +10,15 @@ int main()
   FILE* file;
   int vertexSize = 0;
   char vertexNames[NUM_VERTICES][VERT_NAME_LEN];
-  char edges[NUM_VERTICES][NUM_EDGES][2];
+  char edges[NUM_VERTICES][NUM_EDGES][2][WEIGHT_DIGIT_LEN];
   int edgeSize[NUM_VERTICES] = {0};
-  char testInput[25];
-
+  
   getFileName(fileName);
   file = openFile(fileName);
 
   /* strcpy(vertexNames[50],"Alfa\0"); */
   
-  readData(&vertexSize,vertexNames,edgeSize,edges,file);
+  readData(vertexNames,&vertexSize,edges,edgeSize,file);
   
   /*fscanf(file,"%s",testInput);
 
