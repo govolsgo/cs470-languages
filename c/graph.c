1b2ba3d469cb1c173e70938db0abea97ef75a805
Carter Crews <govolsgo@users.noreply.github.com>
2017-04-26 03:27:41 -0500

    More work done on the read function.

--- c/graph.c
+++ c/graph.c
@@ -42,25 +42,49 @@ FILE* openFile(char fileName[])
   return file;
 }
 
-void readData(int* vertexSize, char vertexNames[NUM_VERTICES][VERT_NAME_LEN],
-	      int* edgeSize, char edges[NUM_VERTICES][NUM_EDGES][2],
-	      FILE* file)
+void readData(char vertexNames[NUM_VERTICES][VERT_NAME_LEN], int* vertexSize, 
+	      char edges[NUM_VERTICES][NUM_EDGES][2][WEIGHT_DIGIT_LEN],
+	      int edgeSize[NUM_VERTICES], FILE* file)
 {
-  char tempVertex[VERT_NAME_LEN];
+  char tempVertex1[VERT_NAME_LEN];
+  char tempVertex2[VERT_NAME_LEN];
+  int weight;
   int vert1Index = -1;
+  int vert2Index = -1;
   
-  fscanf(file,"%s",tempVertex);
-  vert1Index = findVertIndex(tempVertex,*vertexSize,vertexNames);
+  fscanf(file,"%s",tempVertex1);
+  fscanf(file,"%s",tempVertex2);
+  fscanf(file,"%d",&weight);
+  vert1Index = findVertIndex(tempVertex1,*vertexSize,vertexNames);
+  vert2Index = findVertIndex(tempVertex2,*vertexSize,vertexNames);
 
+  printf("tempVertex1: %s\n",tempVertex1);
+  printf("tempVertex2: %s\n",tempVertex2);
+  printf("weight: %d\n",weight);
   printf("Vertex 1 loc: %d\n",vert1Index);
+  printf("Vertex 2 loc: %d\n",vert2Index);
 
-  /*
-  while(!feof(file))
+  while(vert1Index==-1/*!feof(file)*/)
     {
-      if()
+      if(vert1Index == -1)
 	{
-	 
+	  strcpy(vertexNames[*vertexSize],tempVertex1);
+	  vert1Index = *vertexSize;
+	  (*vertexSize)++;
 	}
+      if(vert2Index == -1)
+	{
+	  strcpy(vertexNames[*vertexSize],tempVertex2);
+	  vert2Index = *vertexSize;
+	  (*vertexSize)++;
+	}
+
+      strcpy(edges[vert1Index][edgeSize[vert1Index]][0],tempVertex2);
+      edgeSize[vert1Index]++;
+      
+      
+      printf("vertex[%d]: %s\n",vert1Index,vertexNames[vert1Index]);
+      printf("vertex[%d]: %s\n",vert2Index,vertexNames[vert2Index]);
+      printf("weight: %d\n",weight);
     }
-  */
 }
