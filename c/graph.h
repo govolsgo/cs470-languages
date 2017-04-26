254f8a2153d49ec249ef3b1e05da04cf9ddba70d
Carter Crews <govolsgo@users.noreply.github.com>
2017-04-26 03:27:08 -0500

    Modified the function call to account for storing weight as a C string. Reordered the parameters to easily fit within 80 chars on screen.

--- c/graph.h
+++ c/graph.h
@@ -7,6 +7,6 @@ int findVertIndex(char findMe[], int vertexSize,
 		  char vertexNames[NUM_VERTICES][VERT_NAME_LEN]);
 void getFileName(char fileName[]);
 FILE* openFile(char fileName[]);
-void readData(int* vertexSize, char vertexNames[NUM_VERTICES][VERT_NAME_LEN],
-	      int* edgeSize, char edges[NUM_VERTICES][NUM_EDGES][2],
-	      FILE* file);
+void readData(char vertexNames[NUM_VERTICES][VERT_NAME_LEN], int* vertexSize, 
+	      char edges[NUM_VERTICES][NUM_EDGES][2][WEIGHT_DIGIT_LEN],
+	      int edgeSize[NUM_VERTICES], FILE* file);
