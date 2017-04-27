efcd8590b2ef320502bc4277928e362bef052d54
Carter Crews <govolsgo@users.noreply.github.com>
2017-04-27 09:12:14 -0500

    Finished the readData function. Now working as intended.

--- c/graph.c
+++ c/graph.c
@@ -43,29 +43,26 @@ FILE* openFile(char fileName[])
 }
 
 void readData(char vertexNames[NUM_VERTICES][VERT_NAME_LEN], int* vertexSize, 
-	      char edges[NUM_VERTICES][NUM_EDGES][2][WEIGHT_DIGIT_LEN],
+	      char edges[NUM_VERTICES][NUM_EDGES][2][VERT_NAME_LEN],
 	      int edgeSize[NUM_VERTICES], FILE* file)
 {
   char tempVertex1[VERT_NAME_LEN];
   char tempVertex2[VERT_NAME_LEN];
-  int weight;
+  char weight[WEIGHT_DIGIT_LEN];
   int vert1Index = -1;
   int vert2Index = -1;
-  
+
+  /* Prime the while loop with an initial read. */
   fscanf(file,"%s",tempVertex1);
   fscanf(file,"%s",tempVertex2);
-  fscanf(file,"%d",&weight);
+  fscanf(file,"%s",weight);
   vert1Index = findVertIndex(tempVertex1,*vertexSize,vertexNames);
   vert2Index = findVertIndex(tempVertex2,*vertexSize,vertexNames);
 
-  printf("tempVertex1: %s\n",tempVertex1);
-  printf("tempVertex2: %s\n",tempVertex2);
-  printf("weight: %d\n",weight);
-  printf("Vertex 1 loc: %d\n",vert1Index);
-  printf("Vertex 2 loc: %d\n",vert2Index);
-
-  while(vert1Index==-1/*!feof(file)*/)
+  /* While loop to read in vertices. */
+  while(!feof(file))
     {
+      /* Check if vertices exist in memory. If not, add them. */
       if(vert1Index == -1)
 	{
 	  strcpy(vertexNames[*vertexSize],tempVertex1);
@@ -79,12 +76,23 @@ void readData(char vertexNames[NUM_VERTICES][VERT_NAME_LEN], int* vertexSize,
 	  (*vertexSize)++;
 	}
 
+      /* Copy the data read in to the array of edges. */
       strcpy(edges[vert1Index][edgeSize[vert1Index]][0],tempVertex2);
+      strcpy(edges[vert1Index][edgeSize[vert1Index]][1],weight);
       edgeSize[vert1Index]++;
-      
-      
-      printf("vertex[%d]: %s\n",vert1Index,vertexNames[vert1Index]);
-      printf("vertex[%d]: %s\n",vert2Index,vertexNames[vert2Index]);
-      printf("weight: %d\n",weight);
+
+      /* Make the graph undirected. */
+      strcpy(edges[vert2Index][edgeSize[vert2Index]][0],tempVertex1);
+      strcpy(edges[vert2Index][edgeSize[vert2Index]][1],weight);
+      edgeSize[vert2Index]++;
+
+      /* Next read from file. */
+      fscanf(file,"%s",tempVertex1);
+      fscanf(file,"%s",tempVertex2);
+      fscanf(file,"%s",weight);
+      vert1Index = findVertIndex(tempVertex1,*vertexSize,vertexNames);
+      vert2Index = findVertIndex(tempVertex2,*vertexSize,vertexNames);
     }
+
+  return;
 }
