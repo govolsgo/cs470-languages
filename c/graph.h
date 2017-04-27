a07c22b59af6e37eca8ee8cca2deb4771b4c51f1
EC2 Default User <govolsgo@users.noreply.github.com>
2017-04-27 22:15:03 +0000

    Added declaration for printGraph function.

--- c/graph.h
+++ c/graph.h
@@ -7,6 +7,9 @@ int findVertIndex(char findMe[], int vertexSize,
 		  char vertexNames[NUM_VERTICES][VERT_NAME_LEN]);
 void getFileName(char fileName[]);
 FILE* openFile(char fileName[]);
+void printGraph(char vertexNames[NUM_VERTICES][VERT_NAME_LEN], int vertexSize,
+		char edges[NUM_VERTICES][NUM_EDGES][2][VERT_NAME_LEN],
+		int edgeSize[NUM_VERTICES]);
 void readData(char vertexNames[NUM_VERTICES][VERT_NAME_LEN], int* vertexSize, 
 	      char edges[NUM_VERTICES][NUM_EDGES][2][VERT_NAME_LEN],
 	      int edgeSize[NUM_VERTICES], FILE* file);
